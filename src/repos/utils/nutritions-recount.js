3f5e62004a27749e8cc0bc5d67530f9cae062623
jagi113 <jaroslavgirovsky@gmail.com>
2022-11-05 22:10:00 +0100

    measuring_updated

--- src/repos/utils/nutritions-recount.js
+++ src/repos/utils/nutritions-recount.js
@@ -1,13 +1,21 @@
 measure = (nutrition, amount) => {
-    return nutrition / 100 * amount
+    if (ingredient.unit == "g" || ingredient.unit == "ml") {
+        return nutrition / 100 * amount
+    } else if (ingredient.unit == "kg" || ingredient.unit == "l") {
+        return nutrition * 10 * amount
+    } else if (ingredient.unit == "pl") {
+        return nutrition / 100 * 16 * amount
+    } else if (ingredient.unit == "čl" || ingredient.unit == "kl") {
+        return nutrition / 100 * 5 * amount
+    } else {
+        return nutrition * amount
+    };
 }
 
 module.exports = (ingredients) => {
     ingredients.forEach(ingredient => {
-        if (ingredient.unit == "g" || ingredient.unit == "ml") {
-            nutritions = ["kcal", "protein", "carbohydrate", "sugar", "fats", "saturated_fatty_acids", "transfatty_acids", "monounsaturated_fats", "polyunsaturated_fats", "cholesterol", "fiber", "salt", "water", "calcium", "phe"]
-            nutritions.forEach(nutrition => ingredient[nutrition] = measure(ingredient[nutrition], ingredient.amount))
-        };
+        nutritions = ["kcal", "protein", "carbohydrate", "sugar", "fats", "saturated_fatty_acids", "transfatty_acids", "monounsaturated_fats", "polyunsaturated_fats", "cholesterol", "fiber", "salt", "water", "calcium", "phe"]
+        nutritions.forEach(nutrition => ingredient[nutrition] = measure(ingredient[nutrition], ingredient.amount))
     });
     return ingredients
 }
